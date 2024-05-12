901720927fb316f46ea928c655019a6cde3675ab
Ingmar Heinrich <ingmar.heinrich@gmail.com>
2024-05-12 10:53:17 +0200

    reorg

--- .build/build.bat
+++ .build/build.bat
@@ -8,29 +8,34 @@ if not exist .\environment.bat (
 
 call .\environment.bat
 
-set version=%env_version%
-set modbase=%env_modbase%
-set modpath=%env_modpath%
-set modname=%env_modname%
-set serverip=%env_serverip%
-set serverport=%env_serverport%
-set password=%env_password%
-set playername=%env_playername%
-set modlist=%env_modlist%
-set servermod=%env_servermod%
+set version=0.0.1
+set modbase=P:\inkihh\
+set modpath=
+set modname=AutolockVehicles
+set serverip=192.168.178.96
+set serverport=2302
+set password=inkihh
+set playername=inkihh
+::set modlist=@MuchCarKey;@VPPAdminTools;@Dabs Framework;@CF
+set modlist=@MuchCarKey;@VPPAdminTools;@Dabs Framework;@CF
+::set modlist=@Trader;@VPPAdminTools;@Dabs Framework;@CF
+::set modlist=@DayZ-Expansion-Bundle;@DayZ-Expansion-Core;@DayZ-Expansion-Licensed;@VPPAdminTools;@Dabs Framework;@CF
+set servermod=P:\mods\serverside
 set srcpath=%modbase%%modpath%%modname%
-set dayzclientpath=%env_dayzclientpath%
-set dayzserverpath=%env_dayzserverpath%
-set serverparams=%env_serverparams%
-set buildpath=%env_buildpath%
-set mission=%env_mission%
-set keypath=%env_keypath%
-set clientprofile=%env_clientprofile%
-set serverprofile=%env_serverprofile%
-set serverconfig=%env_serverconfig%
-set pboprojectpath=%env_pboprojectpath%
-set makepbopath=%env_makepbopath%
-set signtoolpath=%env_signtoolpath%
+set modlistlocal=;P:\mods\local\@LBmaster;P:\mods\local\@AutolockVehicles
+set dayzclientpath=C:\Program Files (x86)\Steam\steamapps\common\DayZ
+set dayzserverpath=C:\Program Files (x86)\Steam\steamapps\common\DayZServer
+set clientparams="-dologs" "-nosplash" "-nopause" "-nobenchmark" "-skipIntro"
+set serverparams=/high "-dologs" "-netLog" "-adminlog" "-scriptDebug=true" "-scrAllowFileWrite" "-equalmodrequired"
+set buildpath=P:\mods\local
+set mission=dayzOffline.chernarusplus
+set keypath=C:\Users\ingma\OneDrive\Documents\Github\dayztools\keys\inkihh\inkihh
+set clientprofile=P:\mods\profiles\_dev\%mission%\client
+set serverprofile=P:\mods\profiles\_dev\%mission%\server
+set serverconfig=P:\mods\config\%mission%.dev.cfg
+set pboprojectpath=C:\Program Files (x86)\Mikero\DePboTools\bin\PboProject.exe
+set makepbopath=C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe
+set signtoolpath=C:\Program Files (x86)\Steam\steamapps\common\DayZ Tools\Bin\DsUtils\DSsignfile.exe
 
 set dowipe=0
 
@@ -38,13 +43,12 @@ for /f "tokens=*" %%i in ('node .\modpath.js "%modlist%"') do set fullmodlist=%%
 
 ::goto :end
 
-set fullmodlist=%fullmodlist%%env_modlistlocal%
+set fullmodlist=%fullmodlist%%modlistlocal%
 
 ::goto :nokill
 
 :: STOP THE SERVER AND CLIENT
 echo STOPPING THE SERVER AND CLIENT
-echo .
 TASKKILL /IM DayZ_x64.exe /F /T
 TASKKILL /IM DayZServer_x64.exe /F /T
 TASKKILL /IM DayZDiag_x64.exe /F /T
@@ -63,9 +67,7 @@ call :GetUnixTime unixtime
 
 :: move server logs to logs folder
 echo -------------------------------------------------------
-echo .
 echo MOVING LOGS TO %dayzserverpath%\logs\%unixtime%
-echo .
 mkdir "%dayzserverpath%\logs\%unixtime%"
 move /Y "%serverprofile%\*.log" "%dayzserverpath%\logs\%unixtime%\"
 move /Y "%serverprofile%\*.rpt" "%dayzserverpath%\logs\%unixtime%\"
@@ -74,9 +76,7 @@ move /Y "%serverprofile%\*.adm" "%dayzserverpath%\logs\%unixtime%\
 
 :: delete client logs
 echo -------------------------------------------------------
-echo .
 echo DELETING CLIENT LOGS
-echo .
 del /f /s %clientprofile%\*.log
 del /f /s %clientprofile%\*.rpt
 del /f /s %clientprofile%\*.mdmp
@@ -86,15 +86,11 @@ del /f /s %clientprofile%\*.adm
 
 :: delete old build
 echo -------------------------------------------------------
-echo .
 echo Deleting old build
-echo .
 rmdir /s /q "%buildpath%\%modpath%\@%modname%"
 
 echo -------------------------------------------------------
-echo .
 echo Creating build folders
-echo .
 :: create build folders
 mkdir "%buildpath%\%modpath%@%modname%"
 mkdir "%buildpath%\%modpath%@%modname%\Addons"
@@ -103,23 +99,17 @@ mkdir "%buildpath%\%modpath%@%modname%\Keys"
 
 :: Copy key
 echo -------------------------------------------------------
-echo .
 echo Copying pubkey to Keys/
-echo .
 copy "%keypath%.bikey" "%buildpath%\%modpath%@%modname%\Keys\"
 
 :: PACK THE PBO
 echo -------------------------------------------------------
-echo .
 echo PACKING
-echo .
-cls
 
 start /B "MakePbo" "%makepbopath%" "%srcpath%" "%buildpath%\%modpath%@%modname%\Addons\%modname%.pbo"
 ::start /B "MakePbo" "%makepbopath%" "-X=*-U*.git,*.vscode,*.delete" "%srcpath%" "P:\mods\local\@FINKSmartphone\Addons\%modname%_%version%.pbo"
 ::start /B "PboProject" "%pboprojectpath%" "-S" "+$" "+J" "-W" "+N" "-C" "-K" "%srcpath%" "-M=P:\mods\local\@FINKSmartphone"
 ::start /B "MakePbo" "%makepbopath%" "-W" "-X=*-U*.git,*.vscode,*.delete" "%srcpath%" "P:\mods\local\@FINKSmartphone\Addons\%modname%_%version%.pbo"
-cls
 
 :: -O -Z=default
 TIMEOUT 5
@@ -138,7 +128,6 @@ TASKKILL /IM PboProject.exe /F /T
 ::goto :skipsign
 :: CREATE SIGNED FILE
 echo -------------------------------------------------------
-echo .
 echo SIGNING
 :: @CHANGEME
 start /B  "" "%signtoolpath%" "%keypath%.biprivatekey" "%buildpath%\%modpath%@%modname%\Addons\%modname%.pbo"
@@ -151,9 +140,7 @@ TIMEOUT 1
 
 :: START THE SERVER
 echo -------------------------------------------------------
-echo .
 echo STARTING THE SERVER
-echo .
 cd /d "%dayzserverpath%"
 start "DayZServer" ".\DayZServer_x64.exe" %serverparams% "-port=%serverport%" "-config=%serverconfig%" "-profiles=%serverprofile%"  "-mod=%fullmodlist%" "-servermod=%servermod%"
 ::goto :end
@@ -165,9 +152,7 @@ TIMEOUT 20
 :startclient
 :: START THE CLIENT
 echo -------------------------------------------------------
-echo .
 echo STARTING THE CLIENT
-echo .
 cd /d "%dayzclientpath%"
 start "DayZClient" ".\DayZ_BE.exe" %clientparams% "-name=%playername%" "-password=%password%" "-profiles=%clientprofile%" "-connect=%serverip%" "-port=%serverport%" "-mod=%fullmodlist%"
 
